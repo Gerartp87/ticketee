5d0044b37aa25f862a513f329b91014e783f1f9c
Gerardo TP <gerryk0@hotmail.com>
2011-12-06 02:38:39 -0600

    metiendo admin 1

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,6 +1,29 @@
 require 'spec_helper'
 
 describe ProjectsController do
+
+let(:user) do
+  user = Factory(:user)
+  user.confirm!
+  user
+end
+
+let(:project) { Factory(:project) }
+
+context "standard users" do
+  { "new" => "get",
+    "create" => "post",
+    "edit" => "get",
+    "update" => "put",
+    "destroy" => "delete" }.each do |action, method|
+  it "cannot access the new action" do
+    sign_in(:user, user)
+    get :new
+    response.should redirect_to(root_path)
+    flash[:alert].should eql("You must be an admin to do that.")
+  end
+  end
+end
   
   # Prueba de que si intentas entrar a la web de un proyecto no existente redirecciona a la web principal
   it "displays an error for a missing project" do
