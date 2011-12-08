c977d73ed578072330f84a5e6a76efa811cb85f8
Gerardo TP <gerryk0@hotmail.com>
2011-12-08 02:47:47 -0600

    casi acabando

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 class Admin::UsersController < Admin::BaseController
+  
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
+  
   def index
     @users = User.all(:order => "email")
   end
@@ -6,9 +9,16 @@ class Admin::UsersController < Admin::BaseController
   def new
    @user = User.new
   end
+  
+  def show
+  end
+  
+  def edit
+  end
 
   def create
    @user = User.new(params[:user])
+   set_admin
    if @user.save
      flash[:notice] = "User has been created."
      redirect_to admin_users_path
@@ -16,6 +26,29 @@ class Admin::UsersController < Admin::BaseController
      flash[:alert] = "User has not been created."
      render :action => "new"
    end
+  end
+  
+  def update
+   if params[:user][:password].blank?
+     params[:user].delete(:password)
+   end
+   set_admin
+   if @user.update_attributes(params[:user])
+     flash[:notice] = "User has been updated."
+     redirect_to admin_users_path
+   else
+     flash[:alert] = "User has not been updated."
+     render :action => "edit"
+   end
+  end
+  
+  private
+  def find_user
+    @user = User.find(params[:id])
+  end
+  
+  def set_admin
+    @user.admin = params[:user][:admin] == "1"
   end
   
 end
